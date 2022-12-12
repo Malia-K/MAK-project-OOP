d8507afd57aed295a4d4cb7fe24d47a410034369
Karina <kmirova731@gmail.com>
2022-12-12 12:29:06 +0600

    update check

--- MAK-OOP-PROJECT/src/Controller/ManageUsers.java
+++ MAK-OOP-PROJECT/src/Controller/ManageUsers.java
@@ -1,6 +1,6 @@
 package Controller;
 
-import Model.User;
+import Model.*;
 
 public class ManageUsers {
 	public ManageUsers() {}
@@ -14,32 +14,38 @@ public class ManageUsers {
 		System.out.println(s);
 		
 		System.out.println("Please, choose what kind of user you want to create:"
-				+ "\n\t1. Admin" + "\n\t2. Teacher" + "\n\t3. Student" + "\n\t4. Manager"
-				+ "\n\t5. Librarian" + "\n\t6. Researcher" + "\nChoose number or print 0 if you want to leave:");
+				+ "\n\t1. Admin" + "\n\t2. Teacher" + "\n\t3. Manager" + "\n\t4. Librarian"
+				+ "\n\t5. Student" + "\n\t6. Researcher" + "\nChoose number or print 0 if you want to leave:");
+	}
+	
+	private User createEmployee(int n) {
+		System.out.println("\tExperience");
+		if(n == 1) return createAdmin();
 	}
 	
 	private User createAdmin() {
-		System.out.println("Please, add following information:\n\tFirst Name\n\tLast Name\n\tGender\n\tBirth Date\n\tExperience");
-		return null;
+		Admin a = new Admin();
+		return a;
 	}
 	
-	private void createTeacher() {
+	private User createTeacher() {
+		Teacher t = new Teacher();
+		re
+	}
+	
+	private User createStudent() {
 		
 	}
 	
-	private void createStudent() {
+	private User createManager() {
 		
 	}
 	
-	private void createManager() {
+	private User createLibrarian() {
 		
 	}
 	
-	private void createLibrarian() {
-		
-	}
-	
-	private void createResearcher() {
+	private User createResearcher() {
 		
 	}
 	
@@ -49,20 +55,12 @@ public class ManageUsers {
 		
 		int action = 1;
 		if(action == 0) return;
-		else if(action == 1) {
-			createAdmin();
-		}
-		else if(action == 2) {
-			createTeacher();
-		}
-		else if(action == 3) {
-			createStudent();
-		}
-		else if(action == 4) {
-			createManager();
+		System.out.println("Please, add following information:\n\tFirst Name\n\tLast Name\n\tGender\n\tBirth Date");
+		if(action <= 4) {
+			createEmployee(action);
 		}
 		else if(action == 5) {
-			createLibrarian();
+			createStudent();
 		}
 		else if(action == 6) {
 			createResearcher();
