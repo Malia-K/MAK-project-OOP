5246dba8fbcd5273a16ebbf99f7da243dbd890fb
pp221B030832 <m_kaisagaliev@kbtu.kz>
2022-12-19 14:27:11 +0600

    1.5.7

--- MAK-OOP-PROJECT/src/Model/Teacher.java
+++ MAK-OOP-PROJECT/src/Model/Teacher.java
@@ -2,9 +2,14 @@ package Model;
 
 import enums.*;
 public class Teacher extends Employee {
-    private Faculty faculty;
+	private static final long serialVersionUID = 1L;
+	private Faculty faculty;
     private double rating;
     Schedule schedule;
+    
+    public Teacher() {
+    	
+    }
     
    public Faculty getFaculty() {
         return this.faculty;
@@ -22,5 +27,33 @@ public class Teacher extends Employee {
         this.rating = rating;
     }
     
+    public int compareTo(Teacher o) {
+    	if(this.getRating() == o.getRating()) {
+    		return 0;
+    	}
+    	else if(this.getRating() > o.getRating()) {
+    		return 1;
+    	}
+    	else return 0;
+    }
     
+    public boolean equals(Object o) {
+    	if(this == o) {
+    		return true;
+    	}
+    	if(!super.equals(o)) {
+    		return false;
+    	}
+    	if(getClass() != o.getClass()) {
+    		return false;
+    	}
+    	Teacher other = (Teacher) o;
+    	return getRating() == other.getRating();
+    }
+    
+    public Object clone() throws CloneNotSupportedException{
+    	Teacher copy = (Teacher) super.clone();
+
+    	return copy;
+    }
 }
